bbd4f7aa13dd361617f1849d6d17978f50e91273
Teddy Pascual <teddylayson2000@gmail.com>
2023-08-12 22:51:56 +0800

    updated mobile view

---
+++ build/js/index.js
@@ -0,0 +1,9 @@
+let pics = document.getElementById("myPics");
+let tagged = document.getElementById("myTaggedPics");
+let line = document.getElementById("line");
+function myPics() {
+  line.classList.remove("float-right");
+}
+function myTaggedPics() {
+  line.classList.add("float-right");
+}
