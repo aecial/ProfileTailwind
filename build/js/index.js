907f1870ee8583fa89ed6ae50ab1bfe9b674cb0d
Teddy Pascual <teddylayson2000@gmail.com>
2023-08-14 06:34:01 +0800

    updated css href link

--- build/js/index.js
+++ build/js/index.js
@@ -1,9 +1,11 @@
 let pics = document.getElementById("myPics");
 let tagged = document.getElementById("myTaggedPics");
 let line = document.getElementById("line");
+let picsTab = document.getElementById("taggedPics");
 function myPics() {
   line.classList.remove("float-right");
 }
 function myTaggedPics() {
   line.classList.add("float-right");
+  
 }
