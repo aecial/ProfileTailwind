fe8ecba9d0fc86a27e8734f9f77411017eb2e361
Teddy Pascual <teddylayson2000@gmail.com>
2023-08-14 07:43:38 +0800

    updated pictures sections

--- build/js/index.js
+++ build/js/index.js
@@ -2,10 +2,14 @@ let pics = document.getElementById("myPics");
 let tagged = document.getElementById("myTaggedPics");
 let line = document.getElementById("line");
 let picsTab = document.getElementById("taggedPics");
+let pictures = document.getElementById("pics");
 function myPics() {
   line.classList.remove("float-right");
+  picsTab.classList.add("hidden");
+  pictures.classList.remove("hidden");
 }
 function myTaggedPics() {
   line.classList.add("float-right");
-  
+  picsTab.classList.remove("hidden");
+  pictures.classList.add("hidden");
 }
